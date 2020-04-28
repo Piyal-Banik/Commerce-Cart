78b4701706fdcc6f0d07753740db112ab50eb601
Piyal Banik <piyalbanik2@gmail.com>
2020-04-28 20:31:17 +0530

    Edited Customer Service Component

--- client/src/components/customer-service/customer-service.component.jsx
+++ client/src/components/customer-service/customer-service.component.jsx
@@ -12,41 +12,4 @@ const CustomerService = ({info, image, title, children}) => (
     </div>
 );
 
-/*const CustomerService = ({service, title, info}) => {
-    return(
-        <div className='customer-service'> 
-            <div className='section'>
-                {
-                    service ? <img src='/images/return-logo.png' alt='' /> : ''
-                }
-                <h2 className='title'>Return within 30days</h2>
-                <p className='info'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit</p>
-            </div>
-
-            <div className='divider'></div>
-
-            <div className='section original'>
-                {
-                    service ? <img src='/images/original-logo.png' alt='' /> : ''
-                }
-                
-                <h2 className='title'>100% Original</h2>
-                <p className='info'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit</p>
-            </div>
-
-            <div className='divider'></div>
-
-            <div className='section delivery'>
-                {
-                    service ? <img src='/images/shipping-logo.png' alt='' /> : ''
-                }
-                
-                <h2 className='title'>Get free delivery</h2>
-                <p className='info'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit</p>
-            </div>
-
-        </div>
-    );
-};*/
-
 export default CustomerService;
