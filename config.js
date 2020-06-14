a59495f933532308fb2069ae79fbb8927c8f2569
Piyal Banik <piyalbanik2@gmail.com>
2020-06-14 09:49:58 +0530

    Heroku error fix

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     mongoUrl: `mongodb+srv://username:password@cluster0-v2sts.mongodb.net/CommerceCart?retryWrites=true&w=majority`,
-    secret: '*****'
+    secret: 'secret revealed'
 }
